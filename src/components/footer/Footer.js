1c84d534bfec90ff99acde415f106b3701ef8959
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-01-08 13:11:10 +0200

    Split footer into pieces and add initial tests.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,88 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Footer.css';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import { Translate } from 'react-redux-i18n';
 import HyLogo from "../utilities/HyLogo";
+import ContactDetails from './ContactDetails';
+import FooterLinks from './FooterLinks';
+import Colors from '../utilities/HyColors';
 
 const Footer = () => {
   return (
-    <Container>
+    <Container as="footer">
       <Row>
+        <Col className="text-end">
+          <HyLogo fill={Colors.white} />
+        </Col>
         <Col>
-            <footer className="footer">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4 section left-section">
-                            <div className="row">
-                                <div className="col-md-3">
-                                    <HyLogo />
-                                </div>
-                                <div className="col-md-9">
-                                    <p>
-                                        <strong><Translate value="hy" /> </strong>
-                                    </p>
-                                    <p><Translate value="hy_address_part1" /><br/>
-                                        <Translate value="hy_address_part2" />
-                                    </p>
-                                    <p> <Translate value="hy_switchboard" /></p>
-                                    <p>{('maintenance_info')}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 section middle-section">
-                            <div className="content">
-                                <div className="col-md-9">
-                                    <p>
-                                        <strong>
-                                            <a href={('hy_contact_info_link')} target="_blank" rel="noreferrer noopener">
-                                                <Translate value="hy_contact_info" />
-                                                <span className="screen-reader-only">{('open_in_new_tab')}</span>
-                                            </a>
-                                        </strong>
-                                    </p>
-                                    <p>
-                                        <strong>
-                                            <a href={('hy_terms_of_use_link')} target="_blank" rel="noreferrer noopener">
-                                                <Translate value="hy_terms_of_use" />
-                                                <span className="screen-reader-only">{('open_in_new_tab')}</span>
-                                            </a>
-                                        </strong>
-                                    </p>
-                                    <p>
-                                        <strong>
-                                            <a href={('hy_lataamo_instructions_link')} target="_blank" rel="noreferrer noopener">
-                                                <Translate value="hy_lataamo_instructions" />
-                                                <span className="screen-reader-only">{('open_in_new_tab')}</span>
-                                            </a>
-                                        </strong>
-                                    </p>
-                                    <p>
-                                        <strong>
-                                            <a href={('hy_unitube_katsomo_link')} target="_blank" rel="noreferrer noopener">
-                                                <Translate value="hy_unitube_katsomo" />
-                                                <span className="screen-reader-only">{('open_in_new_tab')}</span>
-                                            </a>
-                                        </strong>
-                                    </p>
-                                    <p>
-                                        <strong>
-                                            <a href='/accessibility_statement.html' target="_blank" rel="noreferrer noopener">
-                                                <Translate value="hy_accessibility_statement" />
-                                                <span className="screen-reader-only">{('open_in_new_tab')}</span>
-                                            </a>
-                                        </strong>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4 section right-section">
-                        </div>
-                    </div>
-                    <div className="clear"></div>
-                </div>
-            </footer>
+          <ContactDetails />
+        </Col>
+        <Col>
+          <FooterLinks />
         </Col>
       </Row>
     </Container>
---
+++ src/components/footer/FooterLinks.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FooterLinks = () => {
+  return (
+    <ul>
+      <li><a href="#">hy_contact_info_link</a></li>
+      <li><a href="#">hy_terms_of_use_link</a></li>
+      <li><a href="#">hy_lataamo_instructions_link</a></li>
+      <li><a href="#">hy_unitube_katsomo_link</a></li>
+      <li><a href="#">hy_accessibility_statement</a></li>
+    </ul>
+  );
+};
+
+FooterLinks.propTypes = {
+};
+
+export default FooterLinks;
