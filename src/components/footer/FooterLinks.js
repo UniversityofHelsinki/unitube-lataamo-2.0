4df800bb69553c02005bd76841924205f4ba2019
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-01-09 12:41:38 +0200

    Use external link icon in headers.

--- src/components/footer/FooterLinks.js
+++ src/components/footer/FooterLinks.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import { ReactComponent as ExternalLinkIcon } from './../utilities/icons/external-link.svg';
+import Colors from './../utilities/HyColors';
+
+const ExternalLink = ({ to, label }) => {
+  return (
+    <>
+      <a href={to} style={{ paddingRight: '8px' }}>{label}</a>
+      <ExternalLinkIcon height={12} width={12} fill={Colors.white} />
+    </>
+  );
+};
 
 const FooterLinks = () => {
+  const { t } = useTranslation();
   return (
     <ul>
-      <li><a href="#">hy_contact_info_link</a></li>
-      <li><a href="#">hy_terms_of_use_link</a></li>
-      <li><a href="#">hy_lataamo_instructions_link</a></li>
-      <li><a href="#">hy_unitube_katsomo_link</a></li>
-      <li><a href="#">hy_accessibility_statement</a></li>
+      <li><ExternalLink to="#" label={t('hy_contact_info_link')} /></li>
+      <li><ExternalLink to="#" label={t('hy_terms_of_use_link')} /></li>
+      <li><ExternalLink to="#" label={t('hy_lataamo_instructions_link')} /></li>
+      <li><ExternalLink to="#" label={t('hy_unitube_katsomo_link')} /></li>
+      <li><ExternalLink to="#" label={t('hy_accessibility_statement')} /></li>
     </ul>
   );
 };
