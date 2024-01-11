f8255a0a3340adcb95b51003e1a5f86efecd749a
anttilan <antti.lankinen@helsinki.fi>
2024-01-11 10:43:29 +0200

    CheckBox.propTypes lisätty

--- src/components/form/CheckBox.test.js
+++ src/components/form/CheckBox.test.js
@@ -3,5 +3,5 @@ import { render } from '@testing-library/react';
 import CheckBox from './CheckBox';
 
 it('renders', () => {
-    render(<CheckBox />);
+    render(<CheckBox label="Valinta" />);
 });
