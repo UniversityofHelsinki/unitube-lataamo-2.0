63b124e701d509e2e6c7fd433b11bbd13c8c7694
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-01-11 07:20:23 +0200

    Initial search bar styling.

--- src/components/left/search/Search.js
+++ src/components/left/search/Search.js
@@ -9,8 +9,8 @@ const Search = () => {
   return (
     <Container>
       <Row>
-        <Col>
-          <input type="text" placeholder="Hae" className="form-control" />
+        <Col className="no-padding">
+          <input type="search" placeholder="Hae" className="form-control" />
         </Col>
       </Row>
       <Row className="mt-1">
