69873cb197202be0317be562842d49db4b01601a
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-01-12 14:08:12 +0200

    Style search according to style guide.

--- src/components/form/InputField.js
+++ src/components/form/InputField.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
+import './InputField.css';
 
 const InputField = ({ placeholder }) => {
     return (
