20fe2231fb9f89c3c5acb2f852061ea31a6ebb05
anttilan <antti.lankinen@helsinki.fi>
2024-01-12 11:15:34 +0200

    Breadcrumb

--- src/components/form/BreadcrumbUrl.test.js
+++ src/components/form/BreadcrumbUrl.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import BreadcrumbUrl from './BreadcrumbUrl';
 
+const crumbs = ['Tallenteet', 'Toka', 'Kolmas'];
+
 it('renders', () => {
-    render(<BreadcrumbUrl />);
+    render(<BreadcrumbUrl crumbs={crumbs}/>);
 });
