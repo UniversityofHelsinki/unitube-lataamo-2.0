f757ee1ab93e73489e259ed7f6d53d783d696d50
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-01-15 07:20:02 +0200

    Remove CheckBox.css for now.

--- src/components/form/CheckBox.js
+++ src/components/form/CheckBox.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import PropTypes from "prop-types";
-import './CheckBox.css';
 
 const CheckBox = ({ label, ...rest }) => {
     return (
