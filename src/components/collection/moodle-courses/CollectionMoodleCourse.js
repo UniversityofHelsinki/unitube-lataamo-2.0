a44151b4d9c54dd4e860c0e17ea7dea75433a7ab
anttilan <antti.lankinen@helsinki.fi>
2024-01-23 13:56:48 +0200

    Toimii, mutta tekemistä vielä.

---
+++ src/components/collection/moodle-courses/CollectionMoodleCourse.js
@@ -0,0 +1,31 @@
+import React, {useId} from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import { ReactComponent as RemoveIcon } from '../../utilities/icons/remove.svg';
+import onKeyDown from "../../accessibility/keydown";
+import PropTypes from "prop-types";
+
+const CollectionMoodleCourse = ({ onRemove, Icon, label }) => {
+    const labelId = useId();
+    return (
+        <Container className="collection-management-right py-1" tabIndex={0} aria-labelledby={labelId}>
+            <Row className="collection-management-right-row justify-content-between align-items-center">
+                <Col className="ps-1 pe-0 collection-management-right-label-icon-col">
+                    <Icon width="30px" height="30px" />
+                </Col>
+                <Col className="text-center">
+                    <p className="mb-0 collection-management-right-label" id={labelId}>{label}</p>
+                </Col>
+                <Col className="text-end px-0 collection-management-right-remove-icon-col">
+                    <RemoveIcon tabIndex={0} role="button" aria-label="..." tabIndex={0} onClick={onRemove} onKeyDown={onKeyDown(onRemove)} width="30px" height="15px" />
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+CollectionMoodleCourse.propTypes = {
+    label: PropTypes.string.isRequired,
+    onRemove: PropTypes.func.isRequired
+};
+
+export default CollectionMoodleCourse;
---
+++ src/components/collection/moodle-courses/CollectionMoodleCourses.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import PropTypes, {number} from 'prop-types';
+import './CollectionMoodleCourses.css';
+import {Button, Col, Container, Row} from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { ReactComponent as UserIcon } from '../../utilities/icons/avatar.svg';
+import FormElementHeader from '../../form/FormElementHeader';
+import CollectionMoodleCourse from "./CollectionMoodleCourse";
+import InputField from "../../form/InputField";
+
+const CollectionMoodleCourses = ({ moodleNumbers = [] }) => {
+    const [selectedMoodlenumbers, setSelectedMoodlenumbers] = useState(moodleNumbers);
+    const [moodlenum, setMoodlenum] = useState({
+        moodleNumber: ''
+    });
+    const { t } = useTranslation();
+    const addMoodleCourse = () => {
+        if (moodlenum && moodlenum?.moodleNumber?.length >0)
+        if (!selectedMoodlenumbers.map(m => m.moodleNumber).includes(moodlenum.moodleNumber)) {
+            setSelectedMoodlenumbers([ ...selectedMoodlenumbers, moodlenum ]);
+        }
+        setMoodlenum({ moodleNumber: '' });
+    };
+    const removeMoodleCourse = (moodleNbr) => {
+        setSelectedMoodlenumbers(selectedMoodlenumbers.filter(m => m.moodleNumber !== moodleNbr.moodleNumber));
+    };
+
+    const containsOnlyNumbers = (event) => {
+        if (/^\d+$/.test(event.target.value)) {
+            return true;
+        }
+        return false;
+    };
+
+    const handleMoodleInputChange = (event) => {
+        if(event.target.value === '' || containsOnlyNumbers(event)) {
+            event.persist();
+            setMoodlenum(() => ({ ...moodlenum, moodleNumber:event.target.value }));
+        }
+    };
+
+    return (
+        <Container className="collection-management-rights">
+            <Row>
+                <FormElementHeader label={t('collection_management_rights_form_header')} />
+            </Row>
+            <Row className="mb-2">
+                <Col>
+                    <span>Mistä löydän kurssitunnistenumeron?</span>
+                </Col>
+            </Row>
+            <Row className="mb-2">
+                <Col>
+                    <InputField type={'text'} label={t('aaa')} placeholder={t('collection_form_name_placeholder')} value={moodlenum.moodleNumber} onChange={handleMoodleInputChange} />
+                </Col>
+                <Col>
+                    <Button className="col btn btn-primary" onClick={addMoodleCourse}>Lisää</Button>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <ul className="collection-management-rights-list">
+                        {selectedMoodlenumbers.map((moodleNbr) =>
+                            <li key={moodleNbr.moodleNumber}>
+                                <CollectionMoodleCourse label={moodleNbr.moodleNumber} onRemove={() => removeMoodleCourse(moodleNbr)} Icon={UserIcon} />
+                            </li>)}
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+CollectionMoodleCourses.propTypes = {
+    moodleNumbers: PropTypes.array,
+};
+
+export default CollectionMoodleCourses;
