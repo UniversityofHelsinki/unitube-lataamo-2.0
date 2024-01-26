d314ce251f92176ce4a8f8d10d4ffcf7348db87b
anttilan <antti.lankinen@helsinki.fi>
2024-01-26 14:05:15 +0200

    Testejä täydennetty.

--- src/components/form/autocomplete/AutoComplete.test.js
+++ src/components/form/autocomplete/AutoComplete.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import AutoComplete from './AutoComplete';
 
-  const options = {};
+  const options = [];
   const placeholder = '';
 
 it('renders', () => {
-  render(<AutoComplete options={} onFilter={() => {}} placeholder={placeholder} onSelect={() => {}} />);
+  render(<AutoComplete options={options} onFilter={() => {}} placeholder={placeholder} onSelect={() => {}} />);
 });
