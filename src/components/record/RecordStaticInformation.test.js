a3f0d9d12ed1b14c5dd1dbabdb4072b402465496
anttilan <antti.lankinen@helsinki.fi>
2024-01-30 14:14:55 +0200

    Tallenteiden latauslinkki komponenttien linkit kuntoon.

--- src/components/record/RecordStaticInformation.test.js
+++ src/components/record/RecordStaticInformation.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import RecordStaticInformation from './RecordStaticInformation';
 
+  const record =
+      { media:
+            [ { url: 'https://unitube.it.helsinki.fi/21EC2020-3AEA-4069-A2DD-08002B30309D',
+             mimetype: 'video/mp4',
+             bitrate: 1923,
+             width: 1280,
+             height: 720,
+             filesize: 33554432,
+             duration: 3600,
+             codec: 'h264',
+             container: 'mp4' } ],
+      };
+
 it('renders', () => {
-  render(<RecordStaticInformation record={{
-    identifier: 'asf-asdf-asd-f'
-  }} />);
+  render(<RecordStaticInformation record={ record } />);
 });
