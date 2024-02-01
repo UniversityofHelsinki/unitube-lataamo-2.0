8e8560e23e1a686d1903cf5985fbcf2c0f06a838
Emmi Sulander <emmi.sulander@helsinki.fi>
2024-02-01 10:33:04 +0200

    Fix tests for Collection Records

--- src/components/collection/records/CollectionRecord.test.js
+++ src/components/collection/records/CollectionRecord.test.js
@@ -3,5 +3,5 @@ import { render } from '@testing-library/react';
 import CollectionRecord from './CollectionRecord';
 
 it('renders', () => {
-  render(<CollectionRecord />);
+  render(<CollectionRecord record={'record'} />);
 });
--- src/components/collection/records/CollectionRecords.test.js
+++ src/components/collection/records/CollectionRecords.test.js
@@ -3,5 +3,5 @@ import { render } from '@testing-library/react';
 import CollectionRecords from './CollectionRecords';
 
 it('renders', () => {
-  render(<CollectionRecords />);
+  render(<CollectionRecords records={['eka', 'toka']} />);
 });
