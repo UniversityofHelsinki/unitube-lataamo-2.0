dc596c8f2315fa778b07b7893cb51a4513186d99
jooaho <jooaho@helsinki.fi>
2024-02-02 13:45:16 +0200

    Accessibility and style changes, add cross-env

--- src/components/record/RecordCollections.js
+++ src/components/record/RecordCollections.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useId} from 'react';
 import PropTypes from 'prop-types';
 import './RecordCollections.css';
 import FormElementHeader from '../form/FormElementHeader';
-import { Col, Container, Form, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import DropDown from '../form/DropDown';
 import { useTranslation } from 'react-i18next';
 import useCollections from "../../hooks/useCollections";
 
 const RecordCollections = ({ onChange, message, disabled = false }) => {
+    const id = useId();
     const { t } = useTranslation();
 
     const [collections, loadingCollections] = useCollections({
@@ -31,12 +32,12 @@ const RecordCollections = ({ onChange, message, disabled = false }) => {
         <Container>
             <Row>
                 <Col>
-                    <FormElementHeader>{t('record_collection_header')}</FormElementHeader>
+                    <FormElementHeader componentId={id}> {t('record_collection_header')} </FormElementHeader>
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <DropDown onChange={(e) => onChange(e.target.value)}  options={(addEmptyCollection() || []).map(asOption)} message={message} disabled={disabled} />
+                    <DropDown id={id} aria-labelledby={id} onChange={(e) => onChange(e.target.value)}  options={(addEmptyCollection() || []).map(asOption)} message={message} disabled={disabled} />
                 </Col>
             </Row>
         </Container>
