8d16cf4faca8f1a943bbae026da296781877b8d9
Vellu <veli-matti.heikkila@helsinki.fi>
2024-02-12 12:50:14 +0200

    added jest-axe to generate component accessibility tests

---
+++ src/components/collection/CollectionCrumb.accessibility.test.js
@@ -0,0 +1,22 @@
+import {render} from "@testing-library/react";
+import {MockProvider} from "../../redux/reducers/MockProvider";
+import CollectionCrumb from "./CollectionCrumb";
+import React from "react";
+import {axe, toHaveNoViolations} from "jest-axe";
+
+// extend expect with toHaveNoViolations function
+expect.extend(toHaveNoViolations);
+
+describe('CollectionCrumb', () => {
+    it('should not have any accessibility violations', async () => {
+
+        const { container } = render(<MockProvider mockReducers={{ location: { path: '', searchParameters: {}} }}>
+            <CollectionCrumb collection={{ id: '123123', title: 'collection' }} />
+        </MockProvider>);
+        const results = await axe(container);
+
+        // use the matcher function in the test
+        expect(results).toHaveNoViolations();
+    });
+});
+
