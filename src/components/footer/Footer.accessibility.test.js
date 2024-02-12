423644a63116b002a184b12ee1b048bd18c59a4a
Vellu <veli-matti.heikkila@helsinki.fi>
2024-02-12 13:00:14 +0200

    added jest-axe to generate component accessibility tests

---
+++ src/components/footer/Footer.accessibility.test.js
@@ -0,0 +1,17 @@
+import {render} from "@testing-library/react";
+import Footer from "./Footer";
+import React from "react";
+import {axe, toHaveNoViolations} from "jest-axe";
+
+// extend expect with toHaveNoViolations function
+expect.extend(toHaveNoViolations);
+describe('Footer', () => {
+    it('should not have any accessibility violations', async () => {
+
+        const { container } = render(<Footer />);
+        const results = await axe(container);
+
+        // use the matcher function in the test
+        expect(results).toHaveNoViolations();
+    });
+});
---
+++ src/components/footer/FooterLinks.accessibility.test.js
@@ -0,0 +1,18 @@
+import {render} from "@testing-library/react";
+import FooterLinks from "./FooterLinks";
+import React from "react";
+import {axe, toHaveNoViolations} from "jest-axe";
+
+// extend expect with toHaveNoViolations function
+expect.extend(toHaveNoViolations);
+describe('FooterLinks', () => {
+    it('should not have any accessibility violations', async () => {
+
+        const { container } = render(<FooterLinks />);
+        const results = await axe(container);
+
+        // use the matcher function in the test
+        expect(results).toHaveNoViolations();
+    });
+});
+
