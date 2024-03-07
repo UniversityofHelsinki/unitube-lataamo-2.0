87dee03d9a0073e20fa9d3ac9bfb6960cde4d521
Vellu <veli-matti.heikkila@helsinki.fi>
2024-03-07 08:31:34 +0200

    change lang attribute on language change

--- src/components/header/Languages.js
+++ src/components/header/Languages.js
@@ -8,14 +8,15 @@ const Languages = () => {
   const { t, i18n } = useTranslation();
   const currentLanguage = i18n.language;
 
-  const onClick = (language) => {
-    i18n.changeLanguage(language);
+  const onClick = async (language) => {
+    document.documentElement.lang = language;
+    await i18n.changeLanguage(language);
   };
 
   return (
     <ul className="languages">
-      {['fi', 'en', 'sv'].map((language) => 
-        <li 
+      {['fi', 'en', 'sv'].map((language) =>
+        <li
           key={language}>
             <Button onClick={() => onClick(language)} variant="link" active={language === currentLanguage}>
               {t(`language_select_${language}`)}
