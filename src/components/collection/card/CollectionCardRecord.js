9684386d4523b191a143c736ac5e0d94f6b9d76a
Vellu <veli-matti.heikkila@helsinki.fi>
2024-03-12 11:19:14 +0200

    get collection card image

--- src/components/collection/card/CollectionCardRecord.js
+++ src/components/collection/card/CollectionCardRecord.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './CollectionCardRecord.css';
 import { Col, Container, Row } from 'react-bootstrap';
+import CollectionCardThumbnail from "./CollectionCardThumbnail";
 
 const CollectionCardRecord = ({ record }) => {
   return (
     <Container className="collection-card-record">
       <Row style={{ height: '100%' }}>
-        <Col className="col-sm-4 collection-card-record-thumb"></Col>
+        <Col className="col-sm-4 collection-card-record-thumb p-0">
+            <CollectionCardThumbnail record={record}></CollectionCardThumbnail>
+        </Col>
         <Col className="col-sm-8 collection-card-record-details">{record.title}</Col>
       </Row>
     </Container>
---
+++ src/components/collection/card/CollectionCardThumbnail.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+
+const CollectionCardThumbnail = ({ record }) => {
+    const [thumbnailUrl, setThumbnailUrl] = useState(null);
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        const fetchThumbnail = async () => {
+            if (record?.cover_image) {
+                const thumbnailUrl = `${process.env.REACT_APP_LATAAMO_THUMBNAIL_SERVER}/thumbnail/v1/${record.cover_image}/50/50`;
+                try {
+                    const response = await fetch(thumbnailUrl);
+                    if (response.ok) {
+                        const data = await response.blob();
+                        setThumbnailUrl(URL.createObjectURL(data));
+                    } else {
+                        throw new Error('Failed to fetch thumbnail');
+                    }
+                } catch (error) {
+                    console.error('Fetch error:', error);
+                }
+            }
+        };
+
+        fetchThumbnail();
+
+        return () => {
+            if (thumbnailUrl) {
+                URL.revokeObjectURL(thumbnailUrl);
+            }
+        };
+    }, [record]);
+
+    const label = t('record_thumbnail_alt_text', { title: record.title });
+
+    const thumbnail = thumbnailUrl ? <img alt={label} title={label} src={thumbnailUrl} /> : <div/>;
+
+    return (
+        <div>
+            {thumbnail}
+        </div>
+    );
+};
+
+CollectionCardThumbnail.propTypes = {
+    record: PropTypes.object.isRequired
+};
+
+export default CollectionCardThumbnail;
