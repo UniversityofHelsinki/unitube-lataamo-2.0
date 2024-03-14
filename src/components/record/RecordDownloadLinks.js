8ec87eb872ab09675f469167964d4c7babbd4bc1
anttilan <antti.lankinen@helsinki.fi>
2024-03-14 13:06:30 +0200

    Videotiedoston laatu

--- src/components/record/RecordDownloadLinks.js
+++ src/components/record/RecordDownloadLinks.js
@@ -7,17 +7,31 @@ import { useTranslation } from 'react-i18next';
 import FormElementHeader from '../form/FormElementHeader';
 import HelpDialog from '../dialog/HelpDialog';
 
-const DownloadLink = ({ to, label }) => {
+const DownloadLink = ({ to, label, resolution, bitrate, size }) => {
   return (
     <>
       <DownloadIcon width="2em" height="2em" />
-      <a download className="ms-2" href={to}>{label}</a>
+      <a download className="ms-2" href={to}>{resolution} - {bitrate} - {size}</a>
     </>
   );
 };
 
-const RecordDownloadLinks = ({ media, publications }) => {
+const getResolution = (mediaFileMetadata) => {
+    return mediaFileMetadata?.streams?.video[0]?.resolution;
+}
+const getBitrate = (mediaFileMetadata) => {
+    return Math.ceil( ((mediaFileMetadata?.size / 1000) * 8) / (mediaFileMetadata?.duration / 1000) ) + ` kbps`;
+}
+const getSize = (mediaFileMetadata) => {
+    return Math.ceil( mediaFileMetadata?.size / 1000000) + ` MB`;
+}
+
+const RecordDownloadLinks = ({ media, publications, mediaFileMetadata }) => {
   const { t } = useTranslation();
+  const resolution = getResolution(mediaFileMetadata);
+  const bitrate = getBitrate(mediaFileMetadata);
+  const size = getSize(mediaFileMetadata);
+
   return (
     <Container>
       <Row>
@@ -39,7 +53,7 @@ const RecordDownloadLinks = ({ media, publications }) => {
           <ul className="blockquote record-download-link-list">
             {media.map((link, i) => (
               <li key={link.to || i}>
-                <DownloadLink to={`${process.env.REACT_APP_LATAAMO_PROXY_SERVER}/api/download/${link.url}`} label={link.type} />
+                <DownloadLink to={`${process.env.REACT_APP_LATAAMO_PROXY_SERVER}/api/download/${link.url}`} label={link.type} resolution={resolution} bitrate={bitrate} size={size} />
               </li>
             ))}
           </ul>
--- src/components/record/RecordStaticInformation.js
+++ src/components/record/RecordStaticInformation.js
@@ -39,7 +39,7 @@ const RecordStaticInformation = ({record, onChange, resetSubtitleDownloadLinks})
             </Row>
             <Row className="mb-4">
                 <Col>
-                    <RecordDownloadLinks media={record.media || []} publications={record.publications || {}} />
+                    <RecordDownloadLinks media={record.media || []} publications={record.publications || {}} mediaFileMetadata={record.mediaFileMetadata}/>
                 </Col>
             </Row>
             {subtitles && subtitles.length > 0 && (
