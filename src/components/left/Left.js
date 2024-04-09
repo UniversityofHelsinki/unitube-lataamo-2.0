bd6176ecb00923dd6434c6ab42ec9c484f1bd4e5
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-04-09 07:23:14 +0300

    - LeftList requires list of lists to iterate for the react key prop

--- src/components/left/Left.js
+++ src/components/left/Left.js
@@ -178,11 +178,11 @@ const Left = () => {
                 if (listElements[path].length > 0) {
                   return listElements[path];
                 }
-                return [
+                return [[
                   <React.Fragment key="empty">
                     {emptyElements[path]}
                   </React.Fragment>
-                ];
+                ]];
               })()}
             </LeftList>
           </Loading>
