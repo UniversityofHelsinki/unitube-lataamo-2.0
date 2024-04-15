1c8f92ee9d5c81683ecf4f71502a833d94d139b3
jooaho <jooaho@helsinki.fi>
2024-04-15 14:22:38 +0300

    Announce inputfield and textarea error messages to screen readers

--- src/components/form/InputField.js
+++ src/components/form/InputField.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, {useId} from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
 import './InputField.css';
 import Message from './Message';
 
 const InputField = ({ message, hideMessage, ...rest }) => {
-
+    const messageId = useId();
     const isInvalid = !!message?.content;
 
   return (
       <>
-        <Form.Control type={ rest.type || "text" } aria-invalid={isInvalid} { ...rest } />
-        {!hideMessage && <Message type={message?.type}>{message?.content}</Message>}
+        <Form.Control type={ rest.type || "text" } aria-invalid={isInvalid} aria-describedby={messageId} { ...rest } />
+        {!hideMessage && <Message messageId={messageId} type={message?.type}>{message?.content}</Message>}
       </>
   );
 };
