f89c6da06ae81e7e15826a470496d769038422e3
jooaho <jooaho@helsinki.fi>
2024-04-18 15:04:04 +0300

    Switched the error page help dialog and content order for an extra smooth accessibility experience

--- src/components/record/RecordErrorPage.js
+++ src/components/record/RecordErrorPage.js
@@ -6,25 +6,25 @@ import RecordActions from './card/RecordActions';
 
 const RecordErrorPage = ({ helpDialog, record, showActions = false, children }) => {
   return (
-    <div className="record-error-page">
-      <div className="record-error-page-logo">
-        <HyLogo />
+      <div className="record-error-page">
+          <div className="record-error-page-logo">
+              <HyLogo/>
+          </div>
+          <div className="record-error-page-content">
+              {children}
+          </div>
+          <div className="record-error-page-help">
+              {helpDialog}
+          </div>
+          <div className="record-error-page-actions">
+              {showActions && <RecordActions record={record} disabled={false}/>}
+          </div>
       </div>
-      <div className="record-error-page-help">
-        {helpDialog}
-      </div>
-      <div className="record-error-page-content">
-        {children}
-      </div>
-      <div className="record-error-page-actions">
-        {showActions && <RecordActions record={record} disabled={false} />}
-      </div>
-    </div>
   );
 };
 
 RecordErrorPage.propTypes = {
-  helpDialog: PropTypes.node.isRequired,
+    helpDialog: PropTypes.node.isRequired,
   record: PropTypes.object,
   showActions: PropTypes.bool,
   children: PropTypes.node
