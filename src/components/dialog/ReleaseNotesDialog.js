403ea0f726800b8d7cf2b99d259fea8cd5b6ab18
Vellu <veli-matti.heikkila@helsinki.fi>
2024-06-11 11:05:53 +0300

    refactor code to release notes dialog component

--- src/components/dialog/ReleaseNotesDialog.js
+++ src/components/dialog/ReleaseNotesDialog.js
@@ -1,23 +1,50 @@
-import React, {useId, useState} from 'react';
+import React, {useId,  useState } from 'react';
 import PropTypes from 'prop-types';
-import {Button, Col, Container, Modal, Row} from 'react-bootstrap';
-import {useTranslation} from 'react-i18next';
+import { Button, Col, Container, Modal, Row } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import Dialog from './Dialog';
 import './HelpDialog.css';
+import useReleaseNotes from "../../hooks/useReleaseNotes";
+import Markdown from "markdown-to-jsx";
+import i18n from "i18next";
 
-const ReleaseNotesDialog = ({ label, releaseNotes }) => {
-    const [show, setShow] = useState(false);
+const modifyDate = (releaseDate) => {
+    const formattedDate = new Intl.DateTimeFormat(i18n.language, {
+        day: '2-digit', month: '2-digit', year: 'numeric'
+    }).format(new Date(releaseDate));
+    return formattedDate;
+};
+
+const ReleaseNotesDialog = ({ label }) => {
     const { t } = useTranslation();
-    const headerId = useId();
+    const releaseNotes = useReleaseNotes();
+    const newestReleaseNotes = [...releaseNotes]
+        .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+        .slice(0, 5);
 
+    const markdownNotes = releaseNotes.length ?
+        newestReleaseNotes.map((note, index) => (
+            <div key={index}>
+                <div className="header">
+                    <h3>{t('version_number')} {note.tag_name}</h3>
+                    <h3>{t('release_date')} {modifyDate(note.released_at)}</h3>
+                </div>
+                <div className="markdown-container">
+                    <Markdown>{note.description}</Markdown>
+                </div>
+            </div>
+        )) : <p>{t('no_new_releases')}</p>;
+
+    const [show, setShow] = useState(false);
+    const headerId = useId();
     const hideDialog = () => setShow(false);
     const toggle = () => setShow(!show);
 
-    const showLink = <Button variant="link" onClick={toggle} className="p-0 help-dialog-button" aria-haspopup="dialog">{label}</Button>;
+    const showLink = <Button variant="link" onClick={toggle} className="p-0 help-dialog-button link-light" aria-haspopup="dialog">{label}</Button>;
 
     return (
         <Dialog showComponent={showLink} show={show} hide={hideDialog} aria-labelledby={headerId}>
-            <Modal.Header id={headerId} closeButton closeLabel={t('help_dialog_close_label')} >
+            <Modal.Header id={headerId} closeButton closeLabel={t('help_dialog_close_label')}>
                 {label}
             </Modal.Header>
             <Modal.Body>
@@ -25,19 +52,24 @@ const ReleaseNotesDialog = ({ label, releaseNotes }) => {
                     <Row>
                         <Col>
                             <div style={{overflowY: 'auto', maxHeight: '50vh'}}>
-                                {releaseNotes}
+                                {markdownNotes}
                             </div>
                         </Col>
                     </Row>
                 </Container>
             </Modal.Body>
         </Dialog>
-);
+    );
 };
 
 ReleaseNotesDialog.propTypes = {
     label: PropTypes.string.isRequired,
-        releaseNotes: PropTypes.array,
+    releaseNotes: PropTypes.arrayOf(
+        PropTypes.shape({
+            version: PropTypes.string,
+            description: PropTypes.string,
+        })
+    ),
 };
 
 export default ReleaseNotesDialog;
--- src/components/footer/FooterLinks.js
+++ src/components/footer/FooterLinks.js
@@ -7,29 +7,8 @@ import useReleaseNotes from "../../hooks/useReleaseNotes";
 import ReleaseNotesDialog from "../dialog/ReleaseNotesDialog";
 import i18n from "i18next";
 
-const modifyDate = (releaseDate) => {
-    const formattedDate = new Intl.DateTimeFormat(i18n.language, {
-        day: '2-digit', month: '2-digit', year: 'numeric'
-    }).format(new Date(releaseDate));
-    return formattedDate;
-};
-
 const FooterLinks = () => {
     const { t } = useTranslation();
-    const releaseNotes = useReleaseNotes();
-    const upcomingReleaseNotes = releaseNotes.filter(note => note.upcoming_release === true);
-    const markdownNotes = upcomingReleaseNotes.length ?
-        upcomingReleaseNotes.map((note, index) => (
-            <div key={index}>
-                <div className="header">
-                    <h4>{t('version_number')} {note.tag_name}</h4>
-                    <h4>{t('release_date')} {modifyDate(note.released_at)}</h4>
-                </div>
-                <div className="markdown-container">
-                    <Markdown>{note.description}</Markdown>
-                </div>
-            </div>
-        )) : <p>{t('no_new_releases')}</p>;
     return (
         <ul>
             <li><ExternalLink to="#" label={t('hy_contact_info_link')} /></li>
@@ -37,7 +16,7 @@ const FooterLinks = () => {
             <li><ExternalLink to="#" label={t('hy_lataamo_instructions_link')} /></li>
             <li><ExternalLink to="#" label={t('hy_unitube_katsomo_link')} /></li>
             <li><ExternalLink to="#" label={t('hy_accessibility_statement')} /></li>
-            <li><ReleaseNotesDialog label={t('release_notes_help_label')} releaseNotes={markdownNotes}></ReleaseNotesDialog></li>
+            <li><ReleaseNotesDialog label={t('release_notes_help_label')}></ReleaseNotesDialog></li>
         </ul>
     );
 };
