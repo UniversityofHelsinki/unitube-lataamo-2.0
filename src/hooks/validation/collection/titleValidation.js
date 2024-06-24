4939b2b10587f1121c7bd17fc4843b3109c60cc5
Riku Hänninen <riku.hanninen@helsinki.fi>
2024-06-24 11:44:41 +0300

    LATAAMO-1188
    - inbox is an illegal word in collection's title

--- src/hooks/validation/collection/titleValidation.js
+++ src/hooks/validation/collection/titleValidation.js
@@ -5,6 +5,11 @@ const validateTitle = (title) => {
   if (!title || title.length === 0) {
     return 'collection_validation_title_is_empty';
   }
+
+  if (title.toLowerCase().includes('inbox')) {
+    return 'collection_validation_title_contains_illegal_word';
+  }
+
   return FIELD_IS_VALID;
 };
 
