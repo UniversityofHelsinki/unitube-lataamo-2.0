0378b4266b7a1e9b19b7cb98b8383f55acee7652
anttilan <antti.lankinen@helsinki.fi>
2025-03-17 11:54:25 +0200

    Pituusrajoitukset.

--- src/hooks/validation/collection/descriptionValidation.js
+++ src/hooks/validation/collection/descriptionValidation.js
@@ -4,6 +4,11 @@ const descriptionValidation = (description) => {
   if (!description || description.length === 0) {
     return 'collection_validation_description_is_empty';
   }
+
+  if (description.length > 1500) {
+    return 'collection_validation_description_is_too_long';
+  }
+
   return FIELD_IS_VALID;
 };
 
